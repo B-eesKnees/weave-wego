bbdf4b5e1b1f4f47451adbf5cf13b9ecbc169aad
Mnung <135577724+Mnung@users.noreply.github.com>
2023-06-27 17:12:19 +0900

    dd

--- vue_front/src/App.vue
+++ vue_front/src/App.vue
@@ -10,7 +10,7 @@
 </template>
 
 <style>
-button {
+ㅇ button {
   cursor: pointer;
 }
 input {
--- vue_front/src/components/Location.vue
+++ vue_front/src/components/Location.vue
@@ -6,15 +6,55 @@ const props = defineProps({
 
 <template>
   <div class="location">
-    <!-- 장소 구역 -->
+    <!-- Location -->
     <div class="location-left">
       <div>{{ location.number }}</div>
       <div class="location-info">
-        <div>{{ location.name }}</div>
+        <div>
+          <a data-bs-toggle="offcanvas" :href="`#${location.name}`">{{
+            location.name
+          }}</a>
+        </div>
         <div class="location-location">{{ location.location }}</div>
       </div>
     </div>
     <div class="location-comment">{{ location.comment }}</div>
+    <!-- Off canvas -->
+    <div
+      class="offcanvas offcanvas-end canvas"
+      tabindex="-1"
+      :id="location.name"
+    >
+      <div class="offcanvas-body canvas-body">
+        <!-- 오프캔버스 내부 내용-->
+        <div class="test">
+          <div>
+            <img
+              src="https://cdn.pixabay.com/photo/2015/12/12/15/24/amsterdam-1089646_1280.jpg"
+            />
+          </div>
+          <div>
+            <div>
+              <img
+                src="https://cdn.pixabay.com/photo/2016/02/17/23/03/usa-1206240_1280.jpg"
+              />
+            </div>
+            <div>
+              <img
+                src="https://cdn.pixabay.com/photo/2016/12/04/19/30/berlin-cathedral-1882397_1280.jpg"
+              />
+            </div>
+          </div>
+        </div>
+        <div class="canvas-name">{{ location.name }}</div>
+        <div class="canvas-location">{{ location.location }}</div>
+        <div class="canvas-detail">
+          <div>연락처 : 02) 123 - 1234</div>
+          <div>홈페이지 : https://www.ezen.co.kr</div>
+          <div>영업시간 :</div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -26,6 +66,7 @@ const props = defineProps({
 .location-left {
   display: flex;
   margin-right: 3rem;
+
   height: 4rem;
   border-bottom: 1px solid black;
   justify-content: space-between;
@@ -34,24 +75,90 @@ const props = defineProps({
 
 .location-left > :first-of-type {
   display: flex;
-  font-size: 2rem;
+  font-size: 1.8rem;
   padding-left: 0.25rem;
   align-items: flex-end;
-  /* 이거 오류나서 end에서 flex-end로 수정함 */
 }
 .location-info {
   display: flex;
   flex-direction: column;
+  justify-content: space-between;
+}
+.location-info a {
+  color: black;
+  text-decoration: none;
+}
+.location-info :first-of-type :hover {
+  color: green;
 }
 .location-info > div:first-of-type {
   font-size: 1.5rem;
-  margin-bottom: 0.75rem;
+  margin-bottom: 0.35rem;
   text-align: end;
   margin-right: 0.25rem;
 }
 .location-location {
-  text-align: end;
+  text-align: flex-end;
   margin-right: 0.25rem;
   font-size: 0.9rem;
+}
+
+.test {
+  width: 100%;
+  height: 24rem;
+  display: flex;
+  margin-bottom: 3rem;
+}
+
+.test > div:first-of-type {
+  width: 66%;
+  height: 100%;
+}
+
+.test > div:last-of-type {
+  width: 34%;
+  height: 100%;
+}
+
+.test > div > div {
+  width: 100%;
+  height: 50%;
+}
+
+.test img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.canvas {
+  width: 36rem !important;
+}
+.canvas-body {
+  padding-left: 0 !important;
+  padding-right: 0 !important;
+  padding-top: 0 !important;
+  margin-top: -3rem !important;
+}
+.canvas-name {
+  font-size: 2rem;
+  font-weight: 500;
+  margin-bottom: 0.25rem;
+  margin-left: 2.5rem;
+}
+.canvas-location {
+  margin-left: 2.5rem;
+  margin-right: 2.5rem;
+  padding-bottom: 1rem;
+  border-bottom: 1px solid black;
+}
+.canvas-detail {
+  font-size: 1.5rem;
+  font-weight: 400;
+  margin-left: 2.5rem;
+}
+
+.canvas-detail > div {
+  margin-top: 3rem;
 }
 </style>
