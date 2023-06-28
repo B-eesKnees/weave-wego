10002163154ba7c5d184ac052a4e039832323f8c
west-play <81891300+west-play@users.noreply.github.com>
2023-06-28 15:12:17 +0900

    v06.28_15:12

--- vue_front/src/components/gnbBar.vue
+++ vue_front/src/components/gnbBar.vue
@@ -1,3 +1,5 @@
+<style src="../assets/css/gnbBar.css"></style>
+<style src="../assets/css/reset.css"></style>
 <template>
   <div class="gnb">
     <div class="gnb_bar">
@@ -6,30 +8,27 @@
           <a href="/"><img src="../assets/img/logo.png" alt="logo" /></a>
         </h2>
       </div>
-      <div v-if="email===null" class="gnb_bar_user">
+      <div v-if="email === null" class="gnb_bar_user">
         <p><a href="/login">로그인</a></p>
         <p><a href="/join">회원가입</a></p>
       </div>
-      <div v-else-if="provider==='kakao'" class="gnb_bar_user">
-        <p>
-          <a href="/mypage"
-            ><img class="kakao_img" :src="image" alt="profileExample"
-          /></a>
-        </p>
+      <div v-else-if="provider === 'kakao'" class="gnb_bar_user_login">
+        <a href="/mypage"
+          ><img class="kakao_img" :src="image" alt="profileExample"
+        /></a>
       </div>
-      <div v-else-if="provider==='local'" class="gnb_bar_user">
-        <p>
-          <a href="/mypage"
-            ><img class="local_img" :src="`http://localhost:3000/downloadProfile/${email}/${image}`" alt="profileExample"
-          /></a>
-        </p>
+      <div v-else-if="provider === 'local'" class="gnb_bar_user_login">
+        <a href="/mypage"
+          ><img
+            class="local_img"
+            :src="`http://localhost:3000/downloadProfile/${email}/${image}`"
+            alt="profileExample"
+        /></a>
       </div>
-      <div v-else-if="provider==='naver'" class="gnb_bar_user">
-        <p>
-          <a href="/mypage"
-            ><img class="naver_img" :src="image" alt="profileExample"
-          /></a>
-        </p>
+      <div v-else-if="provider === 'naver'" class="gnb_bar_user_login">
+        <a href="/mypage"
+          ><img class="naver_img" :src="image" alt="profileExample"
+        /></a>
       </div>
     </div>
   </div>
@@ -39,76 +38,17 @@
 export default {
   data() {
     return {
-      email: '',
-      nick : '',
-      image : '',
-      provider : ''
-    }
+      email: "",
+      nick: "",
+      image: "",
+      provider: "",
+    };
   },
   mounted() {
-    this.email = localStorage.getItem('userID'),
-    this.nick = localStorage.getItem('userNick'),
-    this.image = localStorage.getItem('userImage'),
-    this.provider = localStorage.getItem('userProvider')
-  }
+    (this.email = localStorage.getItem("userID")),
+      (this.nick = localStorage.getItem("userNick")),
+      (this.image = localStorage.getItem("userImage")),
+      (this.provider = localStorage.getItem("userProvider"));
+  },
 };
 </script>
-
-<style scoped>
-a {
-  text-decoration: none;
-  color: black;
-}
-.gnb {
-  z-index: 9999;
-  position: relative;
-}
-.gnb_bar {
-  width: 100%;
-  height: 7%;
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
-  border-bottom: 1px solid rgba(0, 0, 0, 0.3);
-  text-align: center;
-  position: fixed;
-  top: 0;
-  background-color: #fafafa;
-}
-.gnb_bar_logo {
-  position: absolute;
-  left: 40vw;
-}
-.gnb_bar_logo a {
-  padding: 3vh;
-}
-
-.gnb_bar_logo > h2 > a > img {
-  width: 20vh;
-}
-.gnb_bar_user {
-  display: flex;
-  margin-right: 10%;
-}
-.gnb_bar_user a {
-  padding: 2vh;
-}
-
-.gnb_bar_user a:hover {
-  background-color: rgba(56, 130, 101, 0.2);
-}
-.kakao_img {
-  width: 7%;
-  border-radius: 100%;
-}
-
-.local_img {
-  width: 7%;
-  border-radius: 100%;
-}
-
-.naver_img {
-  width: 7%;
-  border-radius: 100%;
-}
-</style>
