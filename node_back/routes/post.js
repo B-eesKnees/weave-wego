25c6fb5e57e700fa4ba9518951aef5499c069dc5
ythdnjs <oxoxox9797@gmail.com>
2023-07-05 17:26:03 +0900

    post.js 주석 제거

--- node_back/routes/post.js
+++ node_back/routes/post.js
@@ -13,29 +13,29 @@ router.get("/board", (req, res) => {
   WHERE b.BRD_ID =2;
   `;
 
-//   db.query(query, [boardId], (err, results) => {
-//     if (err) {
-//       console.error(err);
-//       res.status(500).json({ error: "서버 에러" });
-//     } else {
-//       const board = results[0];
-//       if (!board) {
-//         res.status(404).json({ error: "게시글을 찾을 수 없습니다." });
-//       } else {
-//         if (board.BRD_OPEN) {
-//           res.json({ board });
-//         } else {
-//           const userId = req.user & req.user.id;
-//           if (userId && userId === board.BRD.WRITER) {
-//             res.json({ board });
-//           } else {
-//             res.status(403).json({ error: "비공개 게시글입니다." });
-//           }
-//         }
-//       }
-//     }
-//   });
-// });
+  db.query(query, [boardId], (err, results) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: "서버 에러" });
+    } else {
+      const board = results[0];
+      if (!board) {
+        res.status(404).json({ error: "게시글을 찾을 수 없습니다." });
+      } else {
+        if (board.BRD_OPEN) {
+          res.json({ board });
+        } else {
+          const userId = req.user & req.user.id;
+          if (userId && userId === board.BRD.WRITER) {
+            res.json({ board });
+          } else {
+            res.status(403).json({ error: "비공개 게시글입니다." });
+          }
+        }
+      }
+    }
+  });
+});
 
 //댓글 받아오기
 router.get("/comments", (req, res) => {
