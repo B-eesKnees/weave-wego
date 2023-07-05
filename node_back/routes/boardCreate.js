5ed611805a831742548f384af5a121234d470cb4
minkyeong2 <whitemk4949@gmail.com>
2023-07-05 17:17:15 +0900

    post.js/boardCreate.js

--- node_back/routes/boardCreate.js
+++ node_back/routes/boardCreate.js
@@ -1,37 +1,104 @@
-const express = require('express');
-const db = require('../db'); //db연결
+const express = require("express");
+const db = require("../db"); //db연결
 
 const router = express.Router();
+//본문
+router.post("/boardform", (req, res) => {
+  const {
+    writer,
+    title,
+    loc_rev1,
+    loc_rev2,
+    loc_rev3,
+    loc_rev4,
+    loc_rev5,
+    rev,
+    hashtag,
+    nick,
+    open,
+    created_at,
+  } = req.body;
 
-router.post('/', async(req, res)=> {
-    const course = {
-        BRD_WRITER: req.body.writer,
-        BRD_TITLE: req.body.title,
-        BRD_LOC_REV1 : req.body.loc_rev1,
-        BRD_LOC_REV2 : req.body.loc_rev2,
-        BRD_LOC_REV3 : req.body.loc_rev3,
-        BRD_LOC_REV4 : req.body.loc_rev4,
-        BRD_LOC_REV5 : req.body.loc_rev5,
-        BRD_REV : req.body.rev,
-        BRD_HASHTAG : req.body.hashtag,
-        BRD_NICK : req.body.nick,
-        BRD_OPEN: req.body.open
+  const course = {
+    BRD_WRITER: writer,
+    BRD_TITLE: title,
+    BRD_LOC_REV1: loc_rev1,
+    BRD_LOC_REV2: loc_rev2,
+    BRD_LOC_REV3: loc_rev3,
+    BRD_LOC_REV4: loc_rev4,
+    BRD_LOC_REV5: loc_rev5,
+    BRD_REV: rev,
+    BRD_HASHTAG: hashtag,
+    BRD_NICK: nick,
+    BRD_OPEN: open,
+    BRD_CREATED_AT: created_at,
+  };
+  const query = `INSERT INTO board (BRD_WRITER, BRD_TITLE, BRD_LOC_REV1, BRD_LOC_REV2, BRD_LOC_REV3, BRD_LOC_REV4, BRD_LOC_REV5, BRD_REV, BRD_HASHTAG, BRD_NICK, BRD_OPEN,BRD_CREATED_AT) 
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, DATE_FORMAT(NOW(), '%Y-%m-%d'))`;
+
+  db.query(query, course, (err) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: " 글 작성 중 오류가 발생했습니다." });
+    } else {
+      const redirectPath = "/어쩌고";
+      res.redirect(redirectPath);
     }
-
-    db.query(`insert into weavewego.board set ?`, course, (err)=> {
-        if(err) {
-            res.send({ // 에러 발생 시
-                'code':400,
-                'failed': 'error occurred',
-                'error': err
-            }) 
-        } else {
-            res.send({ //쿼리 실행 성공시
-                'code':200,
-                'message': '글 작성 성공'
-            })
-        }
-    })
+  });
 });
+
+//댓글
+
+router.post("/commentform", (req, res) => {
+  const { num, writer, created_at, nick, image, comment } = req.body;
+
+  const courseComment = {
+    COM_NUM: num,
+    COM_WRITER: writer,
+    COM_CREATED_AT: created_at,
+    COM_NICK: nick,
+    COM_IMAGE: image,
+    COM_COMMENT: comment,
+  };
+
+  const query = `insert into comment (COM_NUM, COM_WRITER,  COM_CREATED_AT, COM_NICK, COM_IMAGE, COM_COMMENT) VALUES (?,?,?,DATE_FORMAT(NOW(), '%Y-%m-%d'),?,?,?)`;
+
+  // 댓글을 데이터베이스에 저장하는 코드
+  db.query(query, courseComment, (err) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: "댓글 작성 중 오류가 발생했습니다." });
+    } else {
+      const redirectPath = "/저쩌고";
+      res.redirect(redirectPath);
+    }
+  });
+});
+
+//장소
+router.post("/locationform", (req, res) => {
+  const { id, name, lat, lng, add } = req.body;
+
+  const courseLocation = {
+    LOC_ID: id,
+    LOC_NAME: name,
+    LOC_LAT: lat,
+    LOC_LNG: lng,
+    LOC_ADD: add,
+  };
+
+  const query = `insert into location (LOC_ID, LOC_NAME ,LOC_LAT ,LOC_LNG ,LOC_ADD) VALUES (?,?,?,?,?)`;
+
+  db.query(query, courseLocation, (err) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: "에러발생" });
+    } else {
+      res.end();
+    }
+  });
+});
+//이미지
+router.post("/imageform", (req, res) => {});
 
 module.exports = router;
