51bea6e22b45da1ca78260bb2c14558481962880
minkyeong2 <whitemk4949@gmail.com>
2023-07-10 09:29:35 +0900

    app.js

--- node_back/routes/updateLike.js
+++ node_back/routes/updateLike.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../db");
-const path = require("path");
-const fs = require("fs");
 
 //좋아요 증가 (감소)
 router.post("/like/:boardId", (req, res) => {
